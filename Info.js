63fd8fcf549cd4f2908b22a7201449d79bc9ac60
Amana07 <143504230+Amana07@users.noreply.github.com>
2024-01-02 18:13:30 +0530

    Update Info.js

--- Info.js
+++ Info.js
@@ -9,7 +9,7 @@ class Info extends Component {
       <div className='Info' style={{ position: 'absolute', top: '10px', right: '10px' }}>
         <h1>Satellite tracker</h1>
         {stations && stations.length > 0 && <p>Total objects: {stations.length}</p>}
-        {refMode === 0 ? <p>ECF mode</p> : <p>ECI mode</p>}
+        {refMode === 1 ? <p>ECF mode</p> : <p>ECI mode</p>}
       </div>
     );
   }
